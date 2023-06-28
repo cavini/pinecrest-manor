234bcde9980c8eee914e771f1e4a7f6533b77cea
Arthur Cavini <arthurcavini@Arthurs-MacBook-Pro.local>
2023-06-28 18:38:23 -0300

    chore: added global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import GlobalStyles from "./styles/GlobalStyles";
 
 const App = () => {
-  return <>Hi</>;
+  return (
+    <>
+      <GlobalStyles />
+      <h1>hi</h1>
+    </>
+  );
 };
 
 export default App;
