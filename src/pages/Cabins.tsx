f8b1d1d5464a433c1a2d5d4a4ee857c74e0d55b4
Arthur Cavini <arthurcavini@Arthurs-MacBook-Pro.local>
2023-08-30 07:53:35 -0300

    chore: added supabase config and started testing integration

--- src/pages/Cabins.tsx
+++ src/pages/Cabins.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Row, { RowType } from "../ui/Row";
 import Heading, { HeadingType } from "../ui/Headings";
+import { getCabins } from "../services/apiCabins";
 
 const Cabins = () => {
+  useEffect(() => {
+    getCabins().then((data) => console.log(data));
+  });
   return (
     <Row type={RowType.horizontal}>
       <Heading as={HeadingType.H1}>All cabins</Heading>
---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://brxwlusifewujzwvgvcb.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJyeHdsdXNpZmV3dWp6d3ZndmNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTMyMjIwMjQsImV4cCI6MjAwODc5ODAyNH0.7PbdB5bDCGDpTliNJAz2QiMMhG8JdQyVHwNam2AUzcE";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
